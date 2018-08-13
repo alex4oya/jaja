7151a0b3166b674c9597daedd5c63b720c13ca95
alex4oya <42258083+alex4oya@users.noreply.github.com>
2018-08-12 21:47:32 -0500

    hi

--- app/src/main/java/desarrollador_alejandro_manilla/cachfox/myapplication/MainActivity.java
+++ app/src/main/java/desarrollador_alejandro_manilla/cachfox/myapplication/MainActivity.java
@@ -1,13 +1,31 @@
 package desarrollador_alejandro_manilla.cachfox.myapplication;
 
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView textView;
+    CountDownTimer countDownTimer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView=(TextView)findViewById(R.id.texto);
+        countDownTimer=new CountDownTimer(2000,500) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+            }
+
+            @Override
+            public void onFinish() {
+                textView.setText("me cagas");
+
+            }
+        }.start();
+
     }
 }
